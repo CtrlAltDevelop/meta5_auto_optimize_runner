4ce79faa657861f804284a26ae38661b13557bd4
Mohammad Zarif <m.zarif1996@gmail.com>
2025-02-27 13:25:21 +0330

    upgrade abstract main class

--- source/common/main_class.py
+++ source/common/main_class.py
@@ -1,35 +1,38 @@
 import sys
 import logging
+from typing import Iterable, Optional, Tuple
 import warnings
 from contextlib import contextmanager
 from pathlib import Path
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
-from tkinter import Tk
+from tkinter import Tk, filedialog
 
 
 class MainClass:
-    """
-    Main application class that handles the core functionality.
-    """
-
-    base_path: Path
-
     def __init__(self, base_path: Path):
         """
-        Initializes the Main class.
+        The function initializes a class instance with a base path, sets up logging, and filters out
+        UserWarning messages related to openpyxl module.
+        
+        :param base_path: The `base_path` parameter is a Path object that represents the base directory
+        where your logs will be stored. It is used to set up the logging configuration for your
+        application
+        :type base_path: Path
         """
         self.base_path = base_path
         self.__logs_dir = self.base_path / 'logs'
         self.__ensure_logs_directory__()
         self.__logger = None
         self.__setup_logging__()
-
         warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
     @contextmanager
     def _tkinter_root(self):
-        """Context manager to handle the Tkinter root window."""
+        """
+        The function creates a hidden Tkinter root window and yields it for use, ensuring it is
+        destroyed after use.
+        """
         root = Tk()
         try:
             root.withdraw()
@@ -39,7 +42,8 @@ class MainClass:
 
     def __ensure_logs_directory__(self):
         """
-        Ensures that the logs directory exists. Creates it if it does not.
+        The function `__ensure_logs_directory__` attempts to create a logs directory and handles
+        exceptions if the creation fails.
         """
         try:
             self.__logs_dir.mkdir(parents=True, exist_ok=True)
@@ -49,10 +53,9 @@ class MainClass:
 
     def __setup_logging__(self):
         """
-        Configures the logging settings for the application, focusing solely on error logging.
-        Logs are stored in the designated logs directory with the current date in the filename.
+        The function sets up logging configuration to capture and handle ERROR and CRITICAL logs in a
+        custom logger with both file and console handlers.
         """
-
         # Create a custom logger
         self.__logger = logging.getLogger(__name__)
         self.__logger.setLevel(logging.ERROR)  # Capture only ERROR and CRITICAL logs
@@ -81,14 +84,15 @@ class MainClass:
 
     def __run__(self):
         """
-        The main method where the core functionality of the application is implemented.
-        To be overridden with actual logic.
+        The `__run__` method is a placeholder in Python that is meant to be overridden with the actual
+        logic of the application.
         """
         pass
 
     def safe_run(self):
         """
-        Executes the run method safely, handling any exceptions that occur.
+        The `safe_run` function logs the start and end of an application's execution, handles
+        exceptions, and prompts the user to exit if the application is packaged with PyInstaller.
         """
         try:
             self.__logger.debug('Application has started running.')
@@ -101,4 +105,69 @@ class MainClass:
         finally:
             if hasattr(sys, '_MEIPASS'):
                 input('Press Enter to exit.')
+    
+    def _get_file_via_dialog(self, title: str, filetypes: Iterable[Tuple[str, str]], optional: bool = False) -> Optional[Path]:
+        """
+        This function opens a file dialog window in Python and returns the selected file path as a Path
+        object, or None if no file is selected.
+        
+        :param title: The `title` parameter is a string that represents the title of the file dialog
+        window that will be displayed to the user when they are prompted to select a file. It typically
+        describes the purpose of selecting the file, such as "Select a File" or "Choose a Document"
+        :type title: str
+        :param filetypes: The `filetypes` parameter in the `_get_file_via_dialog` method is expected to
+        be an iterable of tuples, where each tuple contains two strings. The first string in the tuple
+        represents the description of the file type (e.g., "Text Files"), and the second string
+        represents the file extension
+        :type filetypes: Iterable[Tuple[str, str]]
+        :param optional: The `optional` parameter in the `_get_file_via_dialog` method is a boolean flag
+        that indicates whether selecting a file is optional or required. If `optional` is set to `True`,
+        it means that the user can choose not to select a file and the method will return `None` in,
+        defaults to False
+        :type optional: bool (optional)
+        :return: The function `_get_file_via_dialog` returns an optional `Path` object. If a file is
+        selected via the file dialog, it returns the `Path` object representing the selected file. If no
+        file is selected and the selection is not optional, it raises a `FileNotFoundError`. If no file
+        is selected and the selection is optional, it returns `None`.
+        """
+        logging.debug(f"Opening file dialog: {title}")
+        with self._tkinter_root():
+            file = filedialog.askopenfile(title=title, filetypes=filetypes, initialdir=self.base_path)
+            if not file and not optional:
+                logging.error(f"{title} not selected.")
+                raise FileNotFoundError(f"{title} not selected.")
+            if file:
+                selected_path = Path(file.name)
+                logging.debug(f"File selected: {selected_path}")
+                return selected_path
+            return None
 
+    def _get_folder_via_dialog(self, title: str, optional: bool = False) -> Optional[Path]:
+        """
+        This function opens a folder selection dialog using tkinter in Python and returns the selected
+        folder path as a Path object or None.
+        
+        :param title: The `title` parameter is a string that represents the title of the folder dialog
+        that will be displayed to the user when they are prompted to select a folder
+        :type title: str
+        :param optional: The `optional` parameter in the `_get_folder_via_dialog` method is a boolean
+        flag that indicates whether selecting a folder is optional or required. If `optional` is set to
+        `True`, the user can choose to not select a folder and the method will return `None`. If
+        `optional`, defaults to False
+        :type optional: bool (optional)
+        :return: The function `_get_folder_via_dialog` returns an optional `Path` object. It returns the
+        selected folder path as a `Path` object if a folder is selected via the dialog. If no folder is
+        selected and the selection is not optional, it raises a `FileNotFoundError`. If no folder is
+        selected but the selection is optional, it returns `None`.
+        """
+        logging.debug(f"Opening folder dialog: {title}")
+        with self._tkinter_root():
+            folder = filedialog.askdirectory(title=title, initialdir=self.base_path)
+            if not folder and not optional:
+                logging.error(f"{title} not selected.")
+                raise FileNotFoundError(f"{title} not selected.")
+            if folder:
+                selected_path = Path(folder)
+                logging.debug(f"Folder selected: {selected_path}")
+                return selected_path
+            return None
